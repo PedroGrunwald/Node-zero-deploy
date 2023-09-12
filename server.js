5ce5924834f55b7a80a032c07d7bc85ac8161b10
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-12 15:16:23 -0300

    feat: add post and get

--- server.js
+++ server.js
@@ -1,19 +1,33 @@
 import { fastify } from "fastify";
-import {DatabaseMemory} from './database-memory.JS'
-const server = fastify();
+import { DatabaseMemory } from "./database-memory.js";
 
-server.post("/videos", () => {
-  return "hello world!";
-});  
+const server = fastify();
+const database = new DatabaseMemory();
+
+server.post("/videos", (request, reply) => {
+  const { title, description, duration } = request.body;
+
+  database.create({
+    title: title,
+    description: description,
+    duration: duration,
+  });
+  return reply.status(201).send();
+});
+
 server.get("/videos", () => {
-  return "hello Pedro Grunwald!";
-});  
+  const videos = database.list();
+
+  return videos;
+});
+
 server.put("/videos/:id", () => {
   return "hello Node.Js!";
-});  
+});
+
 server.delete("/videos/:id", () => {
   return "hello Node.Js!";
-});  
+});
 
 server.listen({
   port: 3333,
