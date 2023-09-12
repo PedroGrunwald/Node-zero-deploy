a75b10ca3a0cc6df00b337c398f8b0baa84c452b
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-12 15:33:05 -0300

    feat:add  query params

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ server.post("/videos", (request, reply) => {
   return reply.status(201).send();
 });
 
-server.get("/videos", () => {
-  const videos = database.list();
+server.get("/videos", (request) => {
+  const search = request.query.search;
+  const videos = database.list(search);
 
   return videos;
 });
@@ -34,12 +35,12 @@ server.put("/videos/:id", (request, reply) => {
   return reply.status(204).send();
 });
 
-server.delete("/videos/:id", (request,reply) => {
- const videoId = request.params.id
+server.delete("/videos/:id", (request, reply) => {
+  const videoId = request.params.id;
 
- database.delete(videoId)
+  database.delete(videoId);
 
- return reply.status(204).send()
+  return reply.status(204).send();
 });
 
 server.listen({
