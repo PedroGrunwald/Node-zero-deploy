678da5ac8883432fe5d721faf2d50c2c79f1216e
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-13 14:44:03 -0300

    chore:server listen

--- server.js
+++ server.js
@@ -36,14 +36,14 @@ server.put("/videos/:id", async (request, reply) => {
   return reply.status(204).send();
 });
 
-server.delete("/videos/:id",async  (request, reply) => {
+server.delete("/videos/:id", async (request, reply) => {
   const videoId = request.params.id;
 
- await database.delete(videoId);
+  await database.delete(videoId);
 
   return reply.status(204).send();
 });
 
 server.listen({
-  port: 3333,
+  port: process.env.PORT ?? 3333,
 });
