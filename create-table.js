e245c9ffc2ac22951dd5d198badb701e55b656eb
PedroGrunwald <pedro.grunwald@gmail.com>
2023-09-13 14:05:48 -0300

    chore: fix table sql

--- create-table.js
+++ create-table.js
@@ -1,10 +1,12 @@
 import { sql } from "./db.js";
 
 sql`
-CREATE TABLE videos(
+  CREATE TABLE videos (
+    id TEXT PRIMARY KEY,
     title TEXT,
     description TEXT,
     duration INTEGER
+
 )
 `.then(() => {
   console.log("tabela criada!");
